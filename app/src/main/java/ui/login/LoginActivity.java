a036e9a3f0940041adac1fd98403f94e4cbf46b1
lipesoft <f.rodrigues0907@gmail.com>
2026-03-23 12:01:30 -0300

    initial commit - HabitFlow structure

---
+++ app/src/main/java/ui/login/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.habitflow.ui.login;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.habitflow.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

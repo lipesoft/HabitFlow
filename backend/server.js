3730061bde76aef38bd4a436d53aef85cd4f9e0b
lipesoft <f.rodrigues0907@gmail.com>
2026-03-31 09:27:44 -0300

    Adicionando backend

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./src/config/database");
+const habitRoutes = require("./src/routes/habitRoutes");
+const authRoutes = require("./src/routes/authRoutes");
+
+const app = express();
+
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/habits", habitRoutes);
+app.use("/auth", authRoutes);
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});

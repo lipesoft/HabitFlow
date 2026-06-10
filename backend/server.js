cc8ecd19093446f311c5b9ee0bb4bdc957b4f7a6
Filipe Rodrigues <f.rodrigues0907@gmail.com>
2026-06-10 20:53:53 -0300

    fix: correção erro backend

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./src/config/database");
@@ -15,8 +15,10 @@ app.use(express.json());
 app.use("/habits", habitRoutes);
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 6000;
+app.get("/", (req, res) => res.json({ status: "ok" }));
 
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
